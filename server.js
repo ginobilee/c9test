be83a84c12175752c488f81e0c562ddf358c0d9a
ginobilee <skyleedong@gmail.com>
2017-05-26 01:28:07 +0000

    conplete route control and add index page

--- server.js
+++ server.js
@@ -2,9 +2,39 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-    res.end('Hello World!');
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.get('/[0-9]+$',function(req,res){
+    var nums = +req.url.slice(1);
+    var tStr = new Date(nums);
+    var natuT = tStr.toLocaleDateString();
+    var result = {
+        'unix':nums,
+        'natural':natuT
+    };
+    res.end(JSON.stringify(result));
+    console.log(JSON.stringify(result));
 });
 
-app.listen('8080',function(){
-    console.log('Example app listening on port 8080!');
-});
+app.get('/*',function(req,res){
+    var urls = req.url.slice(1);
+    var strDecode = decodeURI(urls);
+    var d = new Date(strDecode),result;
+    if(!!d.getFullYear()){
+        var unixT = d.getTime(),natuT = d.toLocaleDateString();
+        result = {
+            'unix':unixT,
+            'natural':natuT
+        };
+        res.end(JSON.stringify(result));
+        return;
+    }
+    result = {
+        'unix':null,
+        'natural':null
+    };
+    res.end(JSON.stringify(result));
+})
+
+app.listen('8080');
